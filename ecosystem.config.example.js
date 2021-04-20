41d3d0ddb9dfc3c9acb98e8879b229dce2af1215
bakjonghun <bakjonghun@hikick.kr>
2021-04-20 18:06:08 +0900

    change: dotenv to pm2

---
+++ ecosystem.config.example.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'kickboard-migrator',
+      script: './index.js',
+      env: {
+        LEGACY_URL: 'mqtt://write_your_legacy_mqtt_url',
+        LEGACY_USERNAME: 'write_your_legacy_mqtt_username',
+        LEGACY_PASSWORD: 'write_your_legacy_mqtt_password',
+        TARGET_URL: 'mqtt://write_your_target_mqtt_url',
+        TARGET_USERNAME: 'write_your_target_mqtt_username',
+        TARGET_PASSWORD: 'write_your_target_mqtt_password',
+        NEW_ADDRESS: 'write_your_new_mqtt_address',
+        NEW_USERNAME: 'write_your_new_mqtt_username',
+        NEW_PASSWORD: 'write_your_new_mqtt_password',
+      },
+    },
+  ],
+};
