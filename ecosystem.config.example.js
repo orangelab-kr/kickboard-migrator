8b59fbef95691f4f7102848acedd32202c5da99d
bakjonghun <bakjonghun@hikick.kr>
2021-04-26 16:10:46 +0900

    add: winston & slack webhook

--- ecosystem.config.example.js
+++ ecosystem.config.example.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'kickboard-migrator',
-      script: './index.js',
+      script: './src/index.js',
       env: {
         LEGACY_URL: 'mqtt://write_your_legacy_mqtt_url',
         LEGACY_USERNAME: 'write_your_legacy_mqtt_username',
@@ -13,6 +13,8 @@ module.exports = {
         NEW_ADDRESS: 'write_your_new_mqtt_address',
         NEW_USERNAME: 'write_your_new_mqtt_username',
         NEW_PASSWORD: 'write_your_new_mqtt_password',
+        SLACK_WEBHOOK_URL:
+          'https://hooks.slack.com/services/WRITE_YOUR_WEBHOOK',
       },
     },
   ],
